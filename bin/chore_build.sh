0f0a6394715fce6a6216a60c5c673d56b4aa5826
bwrosen20 <bwrosen20@gmail.com>
2023-09-10 23:54:16 -0400

    Add frontend to bin/build

--- bin/chore_build.sh
+++ bin/chore_build.sh
@@ -1,5 +1,10 @@
 set -o errexit
 
+# builds the front end code
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
